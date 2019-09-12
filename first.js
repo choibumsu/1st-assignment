8b0455729a217b51bb46e2ab73c3e399f018427e
bumsu <bumsu0211@gmail.com>
2019-09-13 00:53:12 +0900

    Add comment

--- first.js
+++ first.js
@@ -11,15 +11,15 @@ const rl = readline.createInterface({
 
 rl.on("line", function (num) {
 	const numInterger = parseInt(num);
-	const re = /[3, 6, 9]/g;
+	const re = /[3, 6, 9]/g; //3 6 9 만 남길 정규표현식
 	let result = 0;
 
 	for (number = 3; number <= numInterger; number++) {
-		ThreeSixNine = number.toString().match(re);
-		if (ThreeSixNine) { result += ThreeSixNine.length };
+		ThreeSixNine = number.toString().match(re); //숫자를 문자로 바꾼 뒤 정규표현식 적용
+		if (ThreeSixNine) { result += ThreeSixNine.length }; //3 6 9만 남은 char string의 길이를 더함
 	}
 	console.log(result);
-	
+
 	rl.close()
 }).on("close", function () {
 	process.exit();
