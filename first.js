9e5f29486ef9a236cbbd857a07ad5fb296d0bab2
bumsu <bumsu0211@gmail.com>
2019-09-12 19:08:10 +0900

    Solve first problem

--- first.js
+++ first.js
@@ -2,15 +2,17 @@
 // 369 게임
 // node first.js 해보면 input output이 어떻게 되는지 알 수 있음
 
-const readline = require("readline");
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
-});
+r.question("", function (num) {
+    const numInterger = parseInt(num);
 
-rl.on("line", function(line) {
-	console.log("Hello Goorm! Your input is", line);
-	rl.close();
-}).on("close", function() {
-	process.exit();
-});
+    let result = 0;
+    for (i = 1; i <= numInterger; i++) {
+        Nums = i.toString().split('');
+        for (j = 0; j < Nums.length; j++) {
+            currNum = Nums[j];
+            result += (currNum == '3' || currNum == '6' || currNum == '9' ? 1 : 0);
+        }
+    }
+    console.log(result);
+    r.close()
+})
