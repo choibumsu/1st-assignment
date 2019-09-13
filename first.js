cc53fdc14d0b8e97a26cd3ac58a816457c71b661
bumsu <bumsu0211@gmail.com>
2019-09-13 14:05:36 +0900

    Change variable names

--- first.js
+++ first.js
@@ -3,7 +3,6 @@
 // node first.js 해보면 input output이 어떻게 되는지 알 수 있음
 
 const readline = require('readline');
-
 const rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout
@@ -11,12 +10,12 @@ const rl = readline.createInterface({
 
 rl.on("line", function (num) {
 	const numInterger = parseInt(num);
-	const re = /[3, 6, 9]/g; //3 6 9 만 남길 정규표현식
+	const re369 = /[3, 6, 9]/g; //3 6 9 만 남길 정규표현식
 	let result = 0;
 
-	for (number = 3; number <= numInterger; number++) {
-		ThreeSixNine = number.toString().match(re); //숫자를 문자로 바꾼 뒤 정규표현식 적용
-		if (ThreeSixNine) { result += ThreeSixNine.length }; //3 6 9만 남은 char string의 길이를 더함
+	for (curr_num = 3; curr_num <= numInterger; curr_num++) {
+		arr369 = curr_num.toString().match(re369); //숫자를 문자로 바꾼 뒤 정규표현식 적용
+		if (arr369) result += arr369.length; //3 6 9만 남은 char string의 길이를 더함
 	}
 	console.log(result);
 
